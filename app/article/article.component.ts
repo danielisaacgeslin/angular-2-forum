7ea881d623674a989098119cd59a65cad67d1c0e
Daniel Geslin <dgeslin@belatrix.com>
2016-09-29 15:24:55 -0300

    update

--- app/article/article.component.ts
+++ app/article/article.component.ts
@@ -11,17 +11,50 @@ import { AjaxService } from '../services/ajax.service';
 export class Article{
   id: number;
   article: IArticle;
+  comments: IComment[] = [];
+  articleTags: ITag[] = [];
   edition: IArticle;
-  tags: ITag[];
-  comments: IComment[];
+  tags: ITag[] = [];
   editEnabled: boolean = true;
 
   constructor(private _ajaxService: AjaxService, private _routeParams: RouteParams){ }
-
   ngOnInit(): void{
     const id: number = Number(this._routeParams.get('id'));
     if(!isNaN(id)){
       this.id = id;
+      this.updateArticle();
+      this.updateComments();
+      this.updateArticleTags();
     }
+    this._ajaxService.getTags().subscribe(tags => this.tags = tags);
   }
+
+  updateArticle(): void{
+    this._ajaxService.getArticle(this.id).subscribe(article => this.article = this.edition = article);
+  }
+
+  updateComments(): void{
+    this._ajaxService.getComments(this.id).subscribe(comments => this.comments = comments);
+  }
+
+  updateArticleTags(): void{
+    this._ajaxService.getArticleTagList(this.id).subscribe(articleTags => this.articleTags = articleTags);
+  }
+
+  filterTags(): ITag[]{
+    let filteredTags: ITag[] = [];
+    if(this.tags.length && this.articleTags.length){
+      filteredTags = this.tags.filter(tag =>{
+        for(let i=0; i<this.articleTags.length; i++){
+          if(this.articleTags[i].id === tag.id){
+            return false;
+          }
+        }
+        return true;
+      });
+    }
+    return <ITag[]>filteredTags;
+  }
+
+
 }
--- app/services/ajax.service.ts
+++ app/services/ajax.service.ts
@@ -3,6 +3,7 @@ import { AdapterService } from './adapter.service';
 import { Http, Response } from 'angular2/http';
 import { IArticle } from '../interfaces/article.interface';
 import { ITag } from '../interfaces/tag.interface';
+import { IComment } from '../interfaces/comment.interface';
 import { Observable } from 'rxjs/Observable';
 
 @Injectable()
@@ -13,25 +14,36 @@ export class AjaxService{
 
   getArticleList(): Observable<IArticle[]>{
     return this._http.get(this.api.concat('getArticleList'))
-    .map((response: Response)=>{
-      const data: IArticle[] = this._adapterService.adaptDBArray(response.json().payload);
-      return <IArticle[]>data;
-    })
+    .map((response: Response)=><IArticle[]>this._adapterService.adaptDBArray(response.json().payload))
     //.do(data=>console.log(data))
+    .catch(this.handleError);
+  }
+
+  getArticle(id: number): Observable<IArticle>{
+    return this._http.get(this.api.concat('getArticle&id=' + id))
+    .map((response: Response)=><IArticle>this._adapterService.adaptDBArray(response.json().payload)[0])
+    .catch(this.handleError);
+  }
+
+  getComments(id: number): Observable<IComment[]>{
+    return this._http.get(this.api.concat('getComments&article_id=' + id))
+    .map((response: Response)=><IComment[]>this._adapterService.adaptDBArray(response.json().payload))
+    .catch(this.handleError);
+  }
+
+  getArticleTagList(id: number): Observable<ITag[]>{
+    return this._http.get(this.api.concat('getArticleTagList&article_id=' + id))
+    .map((response: Response)=><ITag[]>this._adapterService.adaptDBArray(response.json().payload))
     .catch(this.handleError);
   }
 
   getTags(): Observable<ITag[]>{
     return this._http.get(this.api.concat('getTags'))
-    .map((response: Response)=>{
-      const data: ITag[] = this._adapterService.adaptDBArray(response.json().payload);
-      return <ITag[]>data;
-    })
-    //.do(data=>console.log(data))
+    .map((response: Response)=><ITag[]>this._adapterService.adaptDBArray(response.json().payload))
     .catch(this.handleError);
   }
 
   handleError(error: Response){
-    return Observable.throw(error.json().error || 'server error');
+    return Observable.throw(error || 'server error');
   }
 }
